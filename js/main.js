1e063fc1f76d6ed26861be9c24c990aefef82b0e
JooD-E <cho1230915@gmail.com>
2026-05-30 21:31:29 +0900

    메인페이지 작업 및 사진 추가0530

---
+++ js/main.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function (){
+    const albumSwiper = new Swiper('.album-swiper', {
+        effect: 'coverflow',
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: 'auto',
+        loop: true,
+        slideToClickedSlide: true,
+        coverflowEffect:{
+            rotate:0,
+            stretch: -50,
+            depth:200,
+            modifier:1,
+            slideShadows:false,
+        }
+    });
+
+    const textSwiper = new Swiper('.text-swiper', {
+        direction: 'vertical',
+        centeredSlides:true,
+        slidesPerView:3,
+        loop: true,
+        allowTouchMove: false,
+    });
+
+    albumSwiper.on('slideChangeTransitionEnd', function () {
+        const currentRealIndex = albumSwiper.realIndex;
+        
+        if (textSwiper.realIndex !== currentRealIndex) {
+            textSwiper.slideToLoop(currentRealIndex, 300, false); 
+        }
+    });
+})
